db7ce5b952f1dda2e4c72c8f24eab1cf073d1c7b
Igor Manukhov <m_igor97@mail.ru>
2022-10-03 22:24:22 +0300

    added models

---
+++ models/movie.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validator.isURL,
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validator.isURL,
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
