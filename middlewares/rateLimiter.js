9194a187e74b224a5ce00c946a79c397a03a6adf
Igor Manukhov <m_igor97@mail.ru>
2022-10-06 23:34:03 +0300

    limiter set 100

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 module.exports.limiter = rateLimit({
   windowMs: 5 * 60 * 1000,
-  max: 1000,
+  max: 100,
   standardHeaders: true,
   legacyHeaders: false,
 });
