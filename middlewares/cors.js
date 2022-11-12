78593febb4047a36f8b6879a12930b7d294c4c82
Igor Manukhov <m_igor97@mail.ru>
2022-11-12 12:11:53 +0300

    cors movies

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,6 +5,7 @@ const allowedCors = cors({
     'http://localhost:3000',
     'http://localhost:3001',
     'https://api.nomoreparties.co/beatfilm-movies/movies',
+    'https://movies.legend1796.nomoredomains.icu',
   ],
   credentials: true,
 });
